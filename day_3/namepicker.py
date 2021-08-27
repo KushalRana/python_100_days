fd0048839de70a0f99abc135d408e2efcc187e98
Kushal <kushalrana1420@gmail.com>
2021-08-27 12:19:18 +0400

    updated namepicker

--- day_3/namepicker.py
+++ day_3/namepicker.py
@@ -7,4 +7,4 @@ names_strings = input(
     "Enter names to be included in random selection each seperated by comma. "
 )
 names = names_strings.split(", ")
-print(f"Person to pay bills is {names[random.randint(0, len(names))]}")
+print(f"Person to pay bills is {names[random.randint(0, len(names) -1)]}")
